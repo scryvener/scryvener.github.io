5be4857aaa26f7f2ca243d38eff9eba7f8fa1b3e
scryvener <71054893+scryvener@users.noreply.github.com>
2022-08-23 23:51:26 -0700

    HeaderFooterFunctionTest

---
+++ assets/js/HeaderFooterLoad.js
@@ -0,0 +1,26 @@
+
+function pullData(path,callback){
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET",path,true);
+	xhttp.onreadystatechange=function(){
+		if (this.readyState == 4 && this.status == 200) {
+			callback(this);
+		};
+	};
+	xhttp.send()
+}
+
+var xmlDoc;
+function loadData(xml){
+	xmlDoc=xml.responseXML
+
+	 
+	let headerNode=xmlDoc.evaluate("//Header",xmlDoc,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null);
+    let footerNode=xmlDoc.evaluate("//Footer",xmlDoc,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null);
+    
+    console.log(headerNode)
+	
+
+};
+
+pullData("https://scryvener.github.io./Header_Footer.xml",loadData)
