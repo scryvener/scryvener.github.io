595642d28a6c43942ff643d938c07a7d17148a3a
scryvener <71054893+scryvener@users.noreply.github.com>
2022-12-30 00:50:49 -0800

    Update scripting.js

--- scripting.js
+++ scripting.js
@@ -1,7 +1,7 @@
 $('#send').on('click', function(e) {
     e.preventDefault();
-    subject = $('#subject').val();
-    body = $('#body').val();
+    var subject = $('#subject').val();
+    var body = $('#body').val();
 
     console.log("mailto:test@example.com?subject=" + subject + "&body=" + body)
   });
