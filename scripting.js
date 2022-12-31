283005285b72aaa97ecee7b9caa861eb1039a6a5
scryvener <71054893+scryvener@users.noreply.github.com>
2022-12-30 21:15:36 -0800

    Update scripting.js

--- scripting.js
+++ scripting.js
@@ -1,22 +1,33 @@
+function findSelect(name,num){
+
+    let value
+
+    for (let r_id=1;r_id<=num;r_id++){
+        let temp_node=document.getElementById(name+String(r_id))
+
+        temp_bool=temp_node.checked
+
+        if (temp_bool==true){
+            value=r_id
+        }
+    }
+
+    return value
+
+}
+
+
 
 function submitThings(){
     var subject = $('#subject').val();
     var open = $('#open').val();
 
-    var rate_value
-    for (let r_id=1;r_id<=5;r_id++){
-        let temp_node=document.getElementById('rateChoice'+String(r_id))
-
-        temp_bool=temp_node.checked
-
-        if (temp_bool==true){
-            rate_value=r_id
-        }
-    }
-
+    var rate_value= findSelect('RateChoice',5)
+    var rec_value= findSelect('RecChoice',5)
 
 
     console.log("Subject: "+subject)
     console.log("Open Response: "+open)
-    console.log(rate_value)
+    console.log("Rating: "+rate_value)
+    console.log("Recommend: "+rec_value)
 }
