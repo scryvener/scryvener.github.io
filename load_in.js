b8de246e3b7331bffb2470a3cf5dd72c28139471
scryvener <71054893+scryvener@users.noreply.github.com>
2022-12-31 09:00:42 -0800

    Update load_in.js

--- load_in.js
+++ load_in.js
@@ -8,7 +8,7 @@ function genPrompt(){
 
     //need to do cleaning, becauses theres no validation rn
 
-    let prompt=`A user is giving feedback on `+String(subject)+`. The user has given a rating of `+String(rating)+` out of 5 for the question "How would you rate this product", with 5 being the best and 1 being the worst.
+    let prompt=`A user is giving feedback on: `+String(subject)+`. The user has given a rating of `+String(rating)+` out of 5 for the question "How would you rate this product", with 5 being the best and 1 being the worst.
      The user has also given a rating of `+String(rec)+` out of 5 for the question "How likely would you recommend this product to a friend?", with 1 being "unlikely" and 5 being "likely".
      The user has provided the following open ended feedback: "`+String(openResponse)+`". Generate a set of up to 3 questions asking for elaboration on their opinion.`
     return prompt
