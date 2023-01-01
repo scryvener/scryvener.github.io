236a89887a28aa7271b8043e47fa60e06caa787e
scryvener <71054893+scryvener@users.noreply.github.com>
2023-01-01 01:13:06 -0800

    dynQ

--- load_in.js
+++ load_in.js
@@ -22,18 +22,6 @@ function callAPI(){
 
 
 // generate the new questions
-const regexp = /t(e)(st(\d?))/g;
-const str = 'test1test2';
-
-const array = [...str.matchAll(regexp)];
-
-console.log(array[0]);
-// expected output: Array ["test1", "e", "st1", "1"]
-
-console.log(array[1]);
-
-
-
 let temp_resp=`A user is giving feedback on headphones. The user has given a rating of 4 out of 5 for the question "How would you rate this product", with 5 being the best and 1 being the worst. The user has also given a rating of 4 out of 5 for the question "How likely would you recommend this product to a friend?", with 1 being "unlikely" and 5 being "likely". The user has provided the following open ended feedback: " I really liked it but it's a bit expensive. The sound quality can pop sometimes.". Generate a set of up to 3 questions asking for elaboration on their opinion.
 AI:
 
@@ -47,15 +35,15 @@ const reg= /\d{1}.\s(\w.*)/g;
 let re_array=[...temp_resp.matchAll(reg)];
 
 for (let i=0;i<re_array.length;i++){
-    console.log(re_array[i][1])//1 is the actual capture group?
+
+    let newQ=document.getElementById('dynQ'+String(i))
+
+    newQ.innerHTML=re_array[i][1]
+
+    //console.log(re_array[i][1])//1 is the actual capture group?
 }
 
-
+console.log(genPrompt())
 
 //export/show all the data collected
-
-
-let newdoc=document.getElementById('test')
-
-newdoc.innerHTML=genPrompt()
 
