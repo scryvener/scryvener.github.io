c23b3dd958a0e4c03bfa03ff846548a198c7e69b
scryvener <71054893+scryvener@users.noreply.github.com>
2023-01-21 20:39:31 -0800

    initial funct test

---
+++ submit_init.js
@@ -0,0 +1,10 @@
+
+//pulls data from first half of form and fires to serverside in order to query the openAI API
+function submitInit(){
+    let product=$('#productQ').val()
+    let openR=$('#openRQ').val()
+
+    console.log(product)
+    console.log(openR)
+
+}
